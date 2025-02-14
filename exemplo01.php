cf127270d11c314b0e772d7c538f71377383ae85
ana515-create <anabeatrizportela897@gmail.com>
2025-02-14 16:55:13 -0300

    Add files via upload

---
+++ exemplo01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - PHP</title>
+</head>
+<body>
+    <p>
+        oi sou a isadorinha e aninha portelinha </p>
+       <?php
+       echo "<p>oi sou a isadorinha e aninha portelinha</p>";
+    
+       $nome = "isadorinha e aninha portelinha";
+       echo"<p>oi sou a $nome </p>";
+       echo"<p>oi sou a ".$nome."</p>";
+       ?>
+
+</body>
+</html>
