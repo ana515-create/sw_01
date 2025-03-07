a66b130c86321f15fa76b7a26d087a845635df5e
ana515-create <anabeatrizportela897@gmail.com>
2025-03-07 18:33:44 -0300

    Add files via upload

---
+++ aula04/exercicio01.php/exe01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio01</title>
+</head>
+<body>
+    <h1>Formulario</h1>
+    <h2>Coloque as informações abaixo:</h2>
+
+    <form action="/action_page.php">
+    <label for="quantity">Nome:</label>
+        <input type="text" name="" id=""><br>
+        <br>
+  <label for="quantity">Notas (De 0 á 10):</label>
+  <input type="number" id="quantity" name="quantity" min="0" max="10" require>
+  <input type="submit">
+</form>
+</body>
+</html>
