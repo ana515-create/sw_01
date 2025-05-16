6ede34c96e59077ebb7918e66db9305699116791
ana-portela <anabeatrizportela897@gmail.com>
2025-05-16 17:13:23 -0300

    Add files via upload

---
+++ aula10/Conta.class.php
@@ -0,0 +1,33 @@
+<?php
+    Class Conta{
+        public $Nome;
+        public $Cpf;
+        private $Saldo;
+
+        public function Sacar($valor){
+           if(valor>$this->Saldo){
+            return "SALDO INSUFICIENTE";
+           }else{
+            $this->Saldo = $this->Saldo - $Valor;
+            $resultado = "Seu saldo atual é de R$".$this->Saldo;
+            return $resultado;
+           }
+
+
+    
+        }
+        public function Depositar($valor){
+            $this->Saldo = $this->Saldo + $Valor;
+            $resultado = "Seu saldo atual é de R$".$this->Saldo;
+            return $resultado;
+        }
+
+        public function  ConsultarSaldo(){
+            echo "NOME: " . $this->Nome . "<br>";
+            echo "CPF: " . $this->Cpf . "<br>";
+            echo "SALDO: " . $this->Saldo . "<br>";
+        }
+    }
+
+
+?>
