bbc86053be22368d7b3ddb364c13ea5b0d36e50f
ana-portela <anabeatrizportela897@gmail.com>
2025-05-23 16:55:03 -0300

    Add files via upload

---
+++ aula11/exercicio01.php
@@ -0,0 +1,26 @@
+<?php
+  class Produto{
+    private $Nome;
+    private $Preco;
+    private $Quantidade;
+
+    public function __construct($nome, $preco=0, $quantidade=0){
+    $this ->$nome_produto;
+    $this ->$preco_produto;
+    $this ->$quantidade_produto;
+}
+   public function getNome(){
+    return $this->nome;
+   }
+    
+   public function getpreco(){
+    return $this->preco;
+   }
+  public function adicionarEstoque($quantidade_produto){
+    return $this->Quantidade_produto;
+  }
+
+  }
+
+
+?>
